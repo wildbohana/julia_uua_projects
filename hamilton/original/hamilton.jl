56ffecc556aa1eca3a8c9c9f1a7f13d91505fbbf
wildbohana <83251325+wildbohana@users.noreply.github.com>
2021-06-08 23:33:15 +0200

    funkcija

---
+++ hamilton/original/hamilton.jl
@@ -0,0 +1,37 @@
+function NextNode(G, path, position)
+	while true
+		path[position] = (path[position] + 1)%(size(G.AdjMatrix,1) + 1)
+		if path[position] == 0
+			return
+		elseif G.AdjMatrix[path[position-1], path[position]] != 0
+			j = 1
+			while j != position
+				if path[j] == path[position]
+					break
+				end
+				
+				
+				j += 1
+			end
+			
+			if j == position
+				if position < size(G.AdjMatrix,1) || ((position == size(G.AdjMatrix,1)) && G.AdjMatrix[path[size(G.AdjMatrix,1)], path[1]] != 0)
+					return
+				end
+			end
+		end
+	end
+end
+
+function HamiltonCycle(G, path, position)
+	while true
+		NextNode(G, path, position)
+		if path[position] == 0
+			return
+		elseif position == size(G.AdjMatrix, 1)
+			println("Solution: $(path)")
+		else
+			HamiltonCycle(G, path, position + 1)
+		end
+	end
+end
